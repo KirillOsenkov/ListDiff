787208905b87c6484e22ba893a23582daad723d3
Kirill Osenkov <github@osenkov.com>
2018-08-19 19:58:04 -0700

    Add more tests.

--- Tests/ListDiffTests.cs
+++ Tests/ListDiffTests.cs
@@ -6,11 +6,19 @@ namespace ListDiffTests
 	public class Tests {
 		[Theory]
 		[InlineData ("", "", "")]
+		[InlineData ("", "a", "+(a)")]
+		[InlineData ("a", "", "-(a)")]
 		[InlineData ("a", "a", "a")]
 		[InlineData ("a", "b", "-(a)+(b)")]
 		[InlineData ("ab", "ab", "ab")]
 		[InlineData ("abc", "ab", "ab-(c)")]
 		[InlineData ("ab", "abc", "ab+(c)")]
+		[InlineData ("ab", "zab", "+(z)ab")]
+		[InlineData ("ab", "b", "-(a)b")]
+		[InlineData ("abc", "ac", "a-(b)c")]
+		[InlineData ("abc", "a", "a-(b)-(c)")]
+		[InlineData ("abc", "c", "-(a)-(b)c")]
+		[InlineData ("abc", "", "-(a)-(b)-(c)")]
 		public void SimpleCases (string left, string right, string expectedDiff) {
 			var diff = left.Diff (right);
 			Assert.Equal (expectedDiff, diff.ToString ());
